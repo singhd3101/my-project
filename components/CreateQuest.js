c0417f17e7ff3d64e8c32ff213ee8172e2e1e6df
anuroopa18 <tsanuroopa@gmail.com>
2019-03-07 16:51:58 -0500

    Changes

--- components/CreateQuest.js
+++ components/CreateQuest.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {ScrollView,View,Text,TextInput} from 'react-native'
+import FixedHeader from '../elements/FixedHeader.js';
+import {ScrollView,View,Text,TextInput,StyleSheet,ImageBackground} from 'react-native'
 import { FormLabel, FormInput, FormValidationMessage,Card } from 'react-native-elements'
 
 class CreateQuest extends React.Component {
-    static navigationOptions = {screen: 'Quest'}
     constructor(props) {
         super(props)
         this.state ={
@@ -11,18 +11,52 @@ class CreateQuest extends React.Component {
         }
     }
 
-
     updateForm(newState) {
         this.setState(newState);
     }
 
+
+
+
     render() {
         return(
-            <ScrollView style={{padding: 15}}>
-            <Card>
+            <ScrollView>
+            <ImageBackground source={require('../assets/theme1.jpg')} style={{width: '100%', height: '100%'}}>
+            <Card 
+            title="Create Quest" style={{fontSize: 40, fontColor: 'blue', width:'40'}}>
                <Text>Quest Name</Text>
                <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+               <Text>Quest Name</Text>
+               <TextInput style= {{height:26,fontSize: 20, color: '#000', borderBottomWidth:1, borderBottomColor:'#555' }} value={this.state.name} onChangeText={text => this.updateForm({name: text})}/>
+
             </Card>
+            </ImageBackground>
             </ScrollView>
         )
     }
--- elements/FixedHeader.js
+++ elements/FixedHeader.js
@@ -3,9 +3,10 @@ import {Header} from 'react-native-elements'
 
 const FixedHeader = () =>(
     <Header
-        leftComponent={{ 		icon: 'menu', color: '#fff' }}
-        centerComponent={{	text: 'MY TITLE',
-            style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}/>
+        leftComponent={{icon: 'menu',color: '#fff',paddingTop:20 }}
+        centerComponent={{	text: 'Treasure Hunt',
+            style: { color: '#fff', fontWeight:'bold', fontSize:30, fontFamily: "Papyrus",paddingTop:20} }}
+        rightComponent={{ icon: 'home', color: '#fff',paddingTop:20 }}
+        containerStyle={{backgroundColor: '#c9683e', height:120}}/>
 )
 export default FixedHeader
