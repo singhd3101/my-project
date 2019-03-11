cae414f808a4fb998149c246484d507f0c3e25ea
sakshitonwer <sakshitonwer04@gmail.com>
2019-03-11 14:48:30 -0400

    FadeInView: alignSelf:center

--- components/OrganizerView.js
+++ components/OrganizerView.js
@@ -21,14 +21,14 @@ class OrganizerView extends React.Component {
         <ImageBackground source={require('../assets/theme1.jpg')} style={{width: '100%', height: '100%'}}>
         <View>
         <Text style={{fontFamily:"Papyrus",color: '#fff',fontSize:30, marginBottom:40, marginTop: 200, textAlign:"center"}}>Welcome Organizer!</Text>
-        <FadeInView style={{width: 250, height: 50,paddingTop:'1%', backgroundColor: 'powderblue', borderRadius: '10',marginLeft: 60}}>
+        <FadeInView style={{width: 250, height: 50,paddingTop:'1%', backgroundColor: 'powderblue', borderRadius: '10', alignSelf: "center"}}>
         <Button 
          title="Create Quest" 
          type="clear"
          onPress={() => this.props.navigation.navigate('CreateQuest')}
          titleStyle={{fontFamily: "Papyrus", color: '#562547'}}/>   
         </FadeInView>
-        <FadeInView style={{width: 250, height: 50,paddingTop:'1%', marginTop: '2%', backgroundColor: 'powderblue', borderRadius: '10',marginLeft: 60}}>
+        <FadeInView style={{width: 250, height: 50,paddingTop:'1%', marginTop: '2%', backgroundColor: 'powderblue', borderRadius: '10', alignSelf: "center"}}>
         <Button 
         title="Start Quest" 
         onPress={() => this.props.navigation.navigate('QuestHistory')}
