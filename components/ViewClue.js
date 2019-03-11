087d9ba1708955cb3af606bbc2847cac5a2df1de
Divya <singh3101div@gmail.com>
2019-03-11 14:35:18 -0400

    request hint working

--- components/ViewClue.js
+++ components/ViewClue.js
@@ -14,12 +14,19 @@ class ViewClue extends React.Component {
     constructor(props) {
         super(props)
         this.state ={
-            teamName:['Morbid Hunters', 'Bits Please', 'Hakuna Matata', 'Avengers', 'Justice League', 'Gryffindor', 'Ravenclaw', 'Hufflepuff', 'Slytherin'],
+            teamName:['Avengers'],
             chatMessages: [{playerName: "Sam", message:"Hi all, I am at library and I don't think the answer is here"},
                             {playerName: "Adam", message:"lets meet in ccis"},
                         ],
-            clue: 'Clue: somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing '
+            clue: 'Clue: somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing ',
+            hintRequested: false
         }
+    }
+
+    componentDidMount() {
+        const {navigation} = this.props;
+        const index = navigation.getParam("index");
+        console.log("index ", index);
     }
 
     updateForm(newState) {
@@ -32,19 +39,21 @@ class ViewClue extends React.Component {
 
     skip(){
         alert('Warning: No points for this clue will be given to the team');
-        this.props.navigation.navigate('ViewClue');
+        this.props.navigation.navigate('ViewClue', {index: 1});
         this.setState({
+            hintRequested: false,
             clue: 'New clue for the team, New clue for the team, New clue for the team, New clue for the team, New clue for the team, New clue for the team, New clue for the team'
         })
     }
 
     addMessage(msg) {
+        console.log('msg: ', msg);
         this.chatMessages.push(msg);
     }
 
     render() {
 	    let items = this.state.teamName;
-        let team = 'Team '.concat(items[Math.floor(Math.random()*items.length)]);
+        let team = 'Team: '.concat(items[Math.floor(Math.random()*items.length)]);
         return(
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <FixedHeader/>
@@ -52,10 +61,10 @@ class ViewClue extends React.Component {
             <Card containerStyle={{height:500}}>
             <View style={{flexDirection:"row"}}>
                     <View style={{flex:1}}>
-                    <Text style={{justifyContent:'flex-start', fontWeight:'bold'}}>{team}</Text>
+                    <Text style={{justifyContent:'flex-start', fontSize: 20,fontFamily:"Papyrus",color:'#562547'}}>{team}</Text>
                     </View>
                     <View style={{flex:1}}>
-                    <Text style={{justifyContent:'flex-end', fontWeight:'bold', textAlign:'right'}}>Team Score: 200</Text>
+                    <Text style={{justifyContent:'flex-end', textAlign:'right', fontSize: 20,fontFamily:"Papyrus",color:'#562547'}}>Team Score: 200</Text>
                     </View>
                 </View>
                 <View style={{marginTop:20}}></View>
@@ -77,19 +86,18 @@ class ViewClue extends React.Component {
                 </FadeInView>
                 </View>
                 <View style={{marginTop:60}}></View>
+                {this.state.hintRequested ? 
+                <Text style={{fontFamily: "Papyrus", color: '#562547', paddingTop:5, marginBottom:-27}}>Hint: bla bla bla !</Text> : 
                 <View style={{justifyContent: 'space-between', flex: '1', flexDirection: 'row'}}>
                 <FadeInView style={{width: 120, height: 50,paddingTop:'1%', backgroundColor: 'powderblue', 
                 alignItems:'center', borderRadius: '10'}}>
-                <Button 
-                    title="Requst Hint" 
-                    type="clear"
-                    onPress={() => this.sendReview()}
+                <Button title="Requst Hint" type="clear" onPress={() => this.setState({hintRequested : true})}
                     titleStyle={{fontFamily: "Papyrus", color: '#562547'}}/>   
                 </FadeInView>
                 <FadeInView style={{width: 180, height: 50,paddingTop:'6%', alignItems:'center', borderRadius: '10'}}>
                 <Text style={{fontFamily: "Papyrus", color: '#562547'}}>-20 points</Text> 
                 </FadeInView>
-                </View>
+                </View>}
                 <View style={{marginTop:60}}></View>
                 <FadeInView style={{width: 315, height: 50,paddingTop:'1%', backgroundColor: 'orange', 
                 alignItems:'center', borderRadius: '10'}}>
@@ -99,7 +107,7 @@ class ViewClue extends React.Component {
                     onPress={() => this.skip()}
                     titleStyle={{fontFamily: "Papyrus", color: '#562547'}}/>   
                 </FadeInView>
-                <View style={{marginTop:100}}></View>
+                <View style={{marginTop:80}}></View>
                 <Icon name="chat" size={45} color="#ffcf40" onPress={() => this._panel.show()} />
             </Card>
             <SlidingUpPanel ref={c => this._panel = c}>
