c21878f6f1228a821069135c0d33ca9ca75c1e9c
singhd3101 <35316120+singhd3101@users.noreply.github.com>
2019-04-05 16:54:23 -0400

    Delete environment.js

--- environment.js
+++
@@ -1,7 +0,0 @@
-var Environment = {
-    accessKeyId:'AKIAJPUD4AU7L5VRTILQ', 
-    secretAccessKey:'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', 
-    region:'us-east-1',
-}
-
-module.exports = Environment
